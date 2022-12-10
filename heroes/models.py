81bc5280312089eeba48a921dd76149905df7cfb
Volodymyr Vereshchak <diakonx1985@gmail.com>
2022-12-10 14:48:18 +0200

    add requirements.txt

--- heroes/models.py
+++ heroes/models.py
@@ -13,25 +13,24 @@ class Skill(models.Model):
     defense = models.PositiveIntegerField()
     power = models.PositiveIntegerField()
     knowledge = models.PositiveIntegerField()
-    
+
     def __str__(self) -> str:
         return f"Attack: {self.attack} Defense: {self.defense} Power: {self.power} Knowledge: {self.knowledge}"
 
 
 class DescriptionSecondarySkill(models.Model):
     description = models.TextField()
-    
+
     def __str__(self) -> str:
         return self.description
 
 
 class SecondarySkill(models.Model):
-    
     class Level(models.IntegerChoices):
         BASE = 0
         ADVANCED = 1
         EXPERT = 2
-        
+
     name = models.CharField(max_length=32)
     level = models.PositiveIntegerField(choices=Level.choices)
 
