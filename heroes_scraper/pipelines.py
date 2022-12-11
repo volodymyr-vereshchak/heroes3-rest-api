1e5bb7479b7b74b958338899ace9d32c5facceab
Volodymyr Vereshchak <diakonx1985@gmail.com>
2022-12-11 22:50:07 +0200

    change settings and update pipeline for difirent spiders

--- heroes_scraper/pipelines.py
+++ heroes_scraper/pipelines.py
@@ -16,7 +16,8 @@ from django.core.files import File
 class HeroesScraperPipeline:
     @sync_to_async
     def process_item(self, item, spider):
-        serializer = TownSerializer(data={"name": item["name"], "picture_url": File(open(os.path.join(IMAGES_STORE, item["images"][0]["path"]), "rb"))})
-        if serializer.is_valid():
-            serializer.save()
+        if spider.name == "h3town":
+            serializer = TownSerializer(data={"name": item["name"], "picture_url": File(open(os.path.join(IMAGES_STORE, item["images"][0]["path"]), "rb"))})
+            if serializer.is_valid():
+                serializer.save()
         return item
