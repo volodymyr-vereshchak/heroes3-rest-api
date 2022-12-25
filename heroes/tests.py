7afb80624f3c6d4fb0f592192090b5705ccc9c2b
Volodymyr Vereshchak <diakonx1985@gmail.com>
2022-12-25 14:51:05 +0200

    added tests

--- heroes/tests.py
+++ heroes/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.urls import reverse
 
-# Create your tests here.
+HERO_URL_LIST = reverse("heroes:hero-list")
+
+class UnauthenticatedHeroesApiTests(TestCase):
+    def setUp(self) -> None:
+        self.client = APIClient()
+    
+    def test_anon_user_view_list(self):
+        response = self.client.get(HERO_URL_LIST)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+    
+    # def test_anon_user_add(self):
+    #     response = self.client.post(HERO_URL_LIST, data=)
+
