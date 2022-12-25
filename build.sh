3559ee3801cd07b19dc9555cb662fb01b11a7ceb
Volodymyr Vereshchak <diakonx1985@gmail.com>
2022-12-25 19:30:26 +0200

    update build.sh

--- build.sh
+++ build.sh
@@ -4,13 +4,5 @@ set -o errexit
 
 pip install -r requirements.txt
 
-mkdir -p /download/img/full/
-
-adduser --disabled-password --no-create-home hero-user
-
-chown -R hero-user:hero-user /download/
-
-chmod -R 755 /download/
-
 python manage.py migrate
 python manage.py crawl
