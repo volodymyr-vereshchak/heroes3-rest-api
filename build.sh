27ee1accdd65074ff99ee6307ff3ea8952f05920
Volodymyr Vereshchak <diakonx1985@gmail.com>
2022-12-25 19:20:08 +0200

    add user for linux

--- build.sh
+++ build.sh
@@ -4,5 +4,13 @@ set -o errexit
 
 pip install -r requirements.txt
 
+mkdir -p /download/img/full/
+
+adduser --disabled-password --no-create-home hero-user
+
+chown -R hero-user:hero-user /download/
+
+chmod -R 755 /download/
+
 python manage.py migrate
 python manage.py crawl
