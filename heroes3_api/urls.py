8b4fad8db7fa0dd543c1b91788c901827cf09e63
Volodymyr Vereshchak <diakonx1985@gmail.com>
2022-12-26 18:12:19 +0200

    updated static files for whitenoise

--- heroes3_api/urls.py
+++ heroes3_api/urls.py
@@ -28,5 +28,5 @@ urlpatterns = (
         ),
     ]
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    + static(document_root=settings.STATIC_ROOT)
 )
