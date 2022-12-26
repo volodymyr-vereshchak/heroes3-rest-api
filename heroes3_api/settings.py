7f63f1c86efb157b2869795cebb4fcdf1338ab87
Volodymyr Vereshchak <diakonx1985@gmail.com>
2022-12-26 18:21:35 +0200

    updated static files for whitenoise

--- heroes3_api/settings.py
+++ heroes3_api/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (BASE_DIR / "static",)
+STATICFILES_DIRS = (BASE_DIR,)
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = "media/"
